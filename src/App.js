eab3d42a4bae4dd004f9229716aaeef58a9046ca
Glitch (hello-express) <none>
2019-10-17 14:10:38 +0000

    💄🍂 Checkpoint ./src/App.js:692249/803 ./public/index.html:692249/62

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App(){
+const App = () => {
+  const [data, setData] = useState(null)
+   useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('./data/issues.csv')
+      debugger;
+      setData(result);
+    };
+    fetchData();
+  }, []);
+  console.log(data)
   return <div>hi</div>
 }
 
