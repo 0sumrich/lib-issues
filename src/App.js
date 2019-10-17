e9b1fcc601bf0344fa859aa367d08f19f22d63e1
Glitch (hello-express) <none>
2019-10-17 14:22:30 +0000

    👙🏮 Checkpoint ./src/App.js:692249/865

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
+import * as d3 from "d3-fetch";
 
 const App = () => {
-  const [data, setData] = useState(null)
-   useEffect(() => {
+  
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch('./data/issues.csv')
+      const result = await d3.csv("./data/issues.csv");
       debugger;
       setData(result);
     };
     fetchData();
   }, []);
-  console.log(data)
-  return <div>hi</div>
-}
+  
+  console.log(data);
+  
+  return <div>hi</div>;
+};
 
 export default App;
