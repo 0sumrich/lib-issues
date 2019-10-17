b4d0f5ef1a0a0e7b24f0d8e5e8c70917168cd3e7
Glitch (hello-express) <none>
2019-10-17 15:33:31 +0000

    🚣🚘 Checkpoint ./src/index.js:692249/1299 ./src/style/pure-min.css:692249/17315 ./src/App.js:692249/144

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import {csv} from "d3-fetch";
 import issues from './data/issues.csv'
 
+
+const Button = () => <button class="pure-button">A Pure Button</button>
+
 const App = ({dataCsv}) => {  
   const [data, setData] = useState(dataCsv);  
-  return <div>hi</div>;
+  return <div><Button /></div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import issues from './data/issues.csv';
-import { csv} from 'd3-fetch';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import issues from "./data/issues.csv";
+import { csv } from "d3-fetch";
+import './style/pure-min.css'
+
 const render = async () => {
   const data = await csv(issues);
-  ReactDOM.render(<App dataCsv={data}/>, document.getElementById('root'));
-}
+  ReactDOM.render(<App dataCsv={data} />, document.getElementById("root"));
+};
 
-render()
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
