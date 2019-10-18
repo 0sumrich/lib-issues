91638b2c15c71257f47183192ed2e6db71d136dd
Glitch (hello-express) <none>
2019-10-18 12:19:20 +0000

    🐜⛺️ Checkpoint ./src/components/chart.js:692249/1162

--- src/components/chart.js
+++ src/components/chart.js
@@ -9,14 +9,22 @@ import React from "react";
 // Book,
 // Issues
 
-const getUnique = arr => [...new Set(arr)];
-Array.prototype.unique = () => getUnique(this);
-const getColumn = (colName, dataArr) => dataArr.map(o => o[colName]);
+function getUniqueValues(data){
+  const getUnique = arr => [...new Set(arr)];
+  const getColumn = (colName, dataArr) => dataArr.map(o => o[colName]);
+  const unique = str => getUnique(getColumn(str, data))
+  const columns = ['Local authority', 'Site of Loan', 'Type']
+  const res = {}
+  columns.forEach(col => {
+    res[col] = unique(col)
+  })
+  return res
+}
 
 // let unique = [...new Set(myArray)];
 
 const Chart = ({ data }) => {
-  const authorityNames = getColumn("Local authority", data)//.unique();
+  const uniqueValues = getUniqueValues(data)
   debugger;
   return <svg></svg>;
 };
