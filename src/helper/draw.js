2bd4915c519013a17168e0e1f0bda6cdeff20d7a
Glitch (hello-express) <none>
2019-10-21 13:01:10 +0000

    📼🏡 Checkpoint ./src/helper/draw.js:692249/237

--- src/helper/draw.js
+++ src/helper/draw.js
@@ -45,7 +45,7 @@ function draw(data) {
         .select(this)
         .selectAll(".tick text")
         .each(function() {
-          debugger;
+          const text = d3.select(this);
           const words = text
             .text()
             .split(/\s+/)
@@ -56,14 +56,14 @@ function draw(data) {
           let lineNumber = 0;
           const lineHeight = 1.1; // ems
           const y = text.attr("y");
-          const dy = +text.attr("dy");
+          const dy = +text.attr('dy').slice(0, -3)
           let tspan = text
             .text(null)
             .append("tspan")
             .attr("x", 0)
             .attr("y", y)
             .attr("dy", dy + "em");
-          while (word === words.pop()) {
+          while (words.pop()) {
             line.push(word);
             tspan.text(line.join(" "));
             if (tspan.node().getComputedTextLength() > width) {
