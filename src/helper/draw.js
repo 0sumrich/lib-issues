e99c7431060e4371d63c86feccfb43d04a19f9e0
Glitch (hello-express) <none>
2019-10-21 12:37:10 +0000

    🔖👦 Checkpoint ./src/helper/draw.js:692249/69

--- src/helper/draw.js
+++ src/helper/draw.js
@@ -42,24 +42,25 @@ function draw(data) {
     .call(d3.axisBottom(x))
     .each(function() {
       const text = d3.select(this);
+    debugger;
       const words = text
         .text()
         .split(/\s+/)
         .reverse();
-      const width = x.bandwitdh();
+      const width = x.bandwidth();
       let word = "";
       let line = [];
       let lineNumber = 0;
       const lineHeight = 1.1; // ems
       const y = text.attr("y");
-      const dy = parseFloat(text.attr("dy"));
-      const tspan = text
+      const dy = +text.attr("dy");
+      let tspan = text
         .text(null)
         .append("tspan")
         .attr("x", 0)
         .attr("y", y)
         .attr("dy", dy + "em");
-      while ((word = words.pop())) {
+      while ((word === words.pop())) {
         line.push(word);
         tspan.text(line.join(" "));
         if (tspan.node().getComputedTextLength() > width) {
