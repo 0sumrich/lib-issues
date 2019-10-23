89044392bf59a0a00e4cc2d570e4d3c749981e12
Glitch (hello-express) <none>
2019-10-23 14:39:29 +0000

    🎺🚩 Checkpoint ./src/components/select.js:692249/284

--- src/components/select.js
+++ src/components/select.js
@@ -1,13 +1,23 @@
-import React from "react";
+// import React from "react";
 
-const Option = ({ children }) => <option>{children}</option>;
+// const Option = ({ children }) => <option>{children}</option>;
 
-export default ({ options, id, multiple }) => (
-  <div className="pure-form">
-    <select id={id} className="pure-input-1-2 select" multiple={multiple}>
-      {options.map(option => (
-        <Option key={option}>{option}</Option>
-      ))}
-    </select>
-  </div>
-);
+// export default ({ options, id, multiple }) => (
+//   <div className="pure-form">
+//     <select id={id} className="pure-input-1-2 select" multiple={multiple}>
+//       {options.map(option => (
+//         <Option key={option}>{option}</Option>
+//       ))}
+//     </select>
+//   </div>
+// );
+import React from 'react'
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+ 
+// ...
+const options = [
+  { label: 'Thing 1', value: 1},
+  { label: 'Thing 2', value: 2},
+];
+
+<ReactMultiSelectCheckboxes options={options} />
