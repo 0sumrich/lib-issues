23711eba84498b81045aa1119680429ad919ddd5
Glitch (hello-express) <none>
2019-10-31 13:59:50 +0000

    🌗💵 Checkpoint ./src/helper/getUniqueValues.js:692249/1601

--- src/helper/getUniqueValues.js
+++ src/helper/getUniqueValues.js
@@ -2,13 +2,20 @@ const getUnique = arr => [...new Set(arr)];
 const getColumn = (colName, dataArr) => dataArr.map(o => o[colName]);
 const unique = (str, data) => getUnique(getColumn(str, data));
 const columns = data => Object.keys(data[0]);
-const getUniqueValues = data => {
-  debugger;
+const datesFix = data => {
+  for (let i = 0; i < data.length; i++) {
+    const o = data[i];
+    data[i].Dates = { start: o["Count start"], end: o["Count end"] };
+  }
+  return data;
+};
+const getUniqueValues = d => {
+  const data = datesFix(d);
   const res = {};
   columns(data).forEach(col => {
     res[col] = unique(col, data);
   });
   return res;
-}
+};
 
-export {getUnique, getColumn, unique, columns, getUniqueValues}
+export { getUnique, getColumn, unique, columns, getUniqueValues };
