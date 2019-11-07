6df7a25ed7165473dbaf1d7aeedb4acc3913dbcf
Richard Lynch <lynchardrich@gmail.com>
2019-11-07 17:59:15 +0000

    Updates

---
+++ precache-manifest.9ea1c0fa6127c12768a7fad584e06655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e413ede5bd627088ad5c39f947b0fa3",
+    "url": "/lib-issues/index.html"
+  },
+  {
+    "revision": "648438e4f9696536cac3",
+    "url": "/lib-issues/static/css/2.2dab948c.chunk.css"
+  },
+  {
+    "revision": "e3298ca863866005b72f",
+    "url": "/lib-issues/static/css/main.2680c0ba.chunk.css"
+  },
+  {
+    "revision": "648438e4f9696536cac3",
+    "url": "/lib-issues/static/js/2.b33fbf83.chunk.js"
+  },
+  {
+    "revision": "e3298ca863866005b72f",
+    "url": "/lib-issues/static/js/main.7198bb97.chunk.js"
+  },
+  {
+    "revision": "b9c0adb3cbc1193763b3",
+    "url": "/lib-issues/static/js/runtime-main.3dd3899a.js"
+  },
+  {
+    "revision": "b44bac97abd80eadd0751fcd030c82e1",
+    "url": "/lib-issues/static/media/issues.b44bac97.csv"
+  }
+]);
